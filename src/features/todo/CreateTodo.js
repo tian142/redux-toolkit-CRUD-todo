bc92e8b4dbbdc6ef34ae30204e1ae7021422fa58
Tian <tian.qiu142@gmail.com>
2021-02-13 22:35:32 -0800

    add todos are working, next step is to display todos

--- src/features/todo/CreateTodo.js
+++ src/features/todo/CreateTodo.js
@@ -1,15 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { useDispatch } from "react-redux"
+
+import { addTodo } from "./todoSlice"
 
 export const CreateTodo = () => {
+  const [todo, setTodo] = useState("")
+
+  const dispatch = useDispatch()
+
+  const onInputChange = (e) => setTodo(e.target.value)
+
+  const onInputSubmit = () => dispatch(addTodo(todo))
+
   return (
     <div>
-      {" "}
       <div>
         <h1>Todo List</h1>
         <span>
-          <input />
+          <input value={todo} onChange={onInputChange} />
         </span>
-        <button>Add Todo</button>
+        <button onClick={onInputSubmit}>Add Todo</button>
         <hr />
       </div>
     </div>
