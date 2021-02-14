f679f13d25f39c622c4fe4a7cc57b7c1382d1e43
Tian <tian.qiu142@gmail.com>
2021-02-13 22:49:57 -0800

    implemented basic react-router in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css"
 import { CreateTodo } from "./features/todo/CreateTodo"
 import { TodoList } from "./features/todo/TodoList"
+import { SingleTodo } from "./features/todo/SingleTodo"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <CreateTodo />
-      <TodoList />
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <CreateTodo />
+                <TodoList />
+              </>
+            )}
+          />
+          <Route exact path="/todo/:todoId" component={SingleTodo} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/features/todo/SingleTodo.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const SingleTodo = ({ match }) => {
+  return <div>hi</div>
+}
