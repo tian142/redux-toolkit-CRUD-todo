af33fc29b08520c23f2217d7c00ead14caa060bf
Tian <tian.qiu142@gmail.com>
2021-02-13 22:28:43 -0800

    completed store and place in Provider in index

--- src/features/todo/CreateTodo.js
+++ src/features/todo/CreateTodo.js
@@ -1,5 +1,17 @@
 import React from "react"
 
 export const CreateTodo = () => {
-  return <div></div>
+  return (
+    <div>
+      {" "}
+      <div>
+        <h1>Todo List</h1>
+        <span>
+          <input />
+        </span>
+        <button>Add Todo</button>
+        <hr />
+      </div>
+    </div>
+  )
 }
--- src/features/todo/TodoList.js
+++ src/features/todo/TodoList.js
@@ -1,14 +1,5 @@
 import React from "react"
 
 export const TodoList = () => {
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <span>
-        <input />
-      </span>
-      <button>Add Todo</button>
-      <hr />
-    </div>
-  )
+  return <p>hi</p>
 }
