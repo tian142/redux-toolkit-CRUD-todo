f2688546d09085e3cd1d859ecda3c33bd33c7df6
Tian <tian.qiu142@gmail.com>
2021-02-13 22:25:34 -0800

    added TodoList title and input box

---
+++ src/features/todo/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const TodoList = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <span>
+        <input />
+      </span>
+      <button>Add Todo</button>
+      <hr />
+    </div>
+  )
+}
